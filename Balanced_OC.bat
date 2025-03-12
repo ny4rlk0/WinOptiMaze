6236397ccc20509ddac3b64960b91c9c7e11dbf0
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2025-03-12 21:59:07 +0300

    Update Balanced_OC.bat

--- Balanced_OC.bat
+++ Balanced_OC.bat
@@ -18,21 +18,19 @@ goto :eof
 :payload
 
 ::Kodu buraya yaz:: 
-curl -o power_unlock.reg https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/power_unlock.reg
-timeout /t 3
-curl -o NO_OC_WinOptiMaze.pow https://github.com/ny4rlk0/WinOptiMaze/raw/refs/heads/main/NO_OC_WinOptiMaze.pow
-timeout /t 3
-curl -o Balanced_WinOptiMaze.pow https://github.com/ny4rlk0/WinOptiMaze/raw/refs/heads/main/Balanced_WinOptiMaze.pow
-timeout /t 3
-powershell "[regex]::Matches((powercfg -list), 'GUID: ([\w-]+)') | % { powercfg -delete $_.Groups[1].Value }"
-timeout /t 3
-regedit /s power_unlock.reg
-powercfg /import NO_OC_WinOptiMaze.pow
-powercfg /import Balanced_WinOptiMaze.pow
+curl -o %HOMEDRIVE%\power_unlock.reg https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/power_unlock.reg
+curl -o %HOMEDRIVE%\NO_OC_WinOptiMaze.pow https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/NO_OC_WinOptiMaze.pow
+curl -o %HOMEDRIVE%\Balanced_WinOptiMaze.pow https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/Balanced_WinOptiMaze.pow
+powershell -command "([regex]::Matches((powercfg -list), 'GUID: ([\w-]+)') | ForEach-Object { $_.Groups[1].Value }) | ForEach-Object { powercfg -delete $_ }"
+regedit /s %HOMEDRIVE%\power_unlock.reg
+powershell -command "powercfg /import %HOMEDRIVE%\NO_OC_WinOptiMaze.pow"
+timout /t 1
+powershell -command "powercfg /import %HOMEDRIVE%\Balanced_WinOptiMaze.pow"
+timout /t 1
 for /f "tokens=4" %%f in ('powercfg -list ^| findstr /C:"Balanced_WinOptiMaze"') do set GUID=%%f
 powercfg /S %GUID%
-del power_unlock.reg
-del NO_OC_WinOptiMaze.pow
-del Balanced_WinOptiMaze.pow
+del %HOMEDRIVE%\power_unlock.reg
+del %HOMEDRIVE%\NO_OC_WinOptiMaze.pow
+del %HOMEDRIVE%\Balanced_WinOptiMaze.pow
 start /b "" cmd /c del "%~f0"&exit /b
 ::Kodu buraya yaz:: 
