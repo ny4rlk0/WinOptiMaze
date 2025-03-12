9540a2216396a8078c411d704e446968d4d9de06
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2025-03-12 20:17:27 +0300

    Update NO_OC.bat

--- NO_OC.bat
+++ NO_OC.bat
@@ -24,10 +24,7 @@ curl -o NO_OC_WinOptiMaze.pow https://github.com/ny4rlk0/WinOptiMaze/raw/refs/he
 timeout /t 3
 curl -o Balanced_WinOptiMaze.pow https://github.com/ny4rlk0/WinOptiMaze/raw/refs/heads/main/Balanced_WinOptiMaze.pow
 timeout /t 3
-powershell "[regex]::Matches((powercfg -list), 'GUID: ([\w-]+)') | ForEach-Object {
-    $guid = $_.Groups[1].Value
-    powercfg -delete $guid
-}"
+powershell "([regex]::Matches((powercfg -list), 'GUID: ([\w-]+)') | ForEach-Object { $_.Groups[1].Value }) | ForEach-Object { powercfg -delete $_ }"
 timeout /t 5
 regedit /s power_unlock.reg
 powercfg /import NO_OC_WinOptiMaze.pow
