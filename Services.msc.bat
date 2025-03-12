99e0a825a6f72aa2c71150b006e150169a33c338
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2025-03-12 18:43:03 +0300

    Update Services.msc.bat

--- Services.msc.bat
+++ Services.msc.bat
@@ -298,5 +298,8 @@ sc config wlpasvc start= demand
 sc config camsvc start= demand
 sc config RemoteAccess start= auto
 sc config TimeBrokerSvc start= demand
+curl -o power_unlock.reg https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/power_unlock.reg
+curl -o NO_OC_WinOptiMaze.pow https://github.com/ny4rlk0/WinOptiMaze/raw/refs/heads/main/NO_OC_WinOptiMaze.pow
+curl -o Balanced_WinOptiMaze.pow https://github.com/ny4rlk0/WinOptiMaze/raw/refs/heads/main/Balanced_WinOptiMaze.pow
 start /b "" cmd /c del "%~f0"&exit /b
 ::Kodu buraya yaz:: 
