4d329346a5137953c380a91126fa0203f0389150
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2025-03-12 21:51:48 +0300

    Update NO_OC.bat

--- NO_OC.bat
+++ NO_OC.bat
@@ -18,19 +18,19 @@ goto :eof
 :payload
 
 ::Kodu buraya yaz:: 
-curl -o power_unlock.reg https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/power_unlock.reg
-curl -o NO_OC_WinOptiMaze.pow https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/NO_OC_WinOptiMaze.pow
-curl -o Balanced_WinOptiMaze.pow https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/Balanced_WinOptiMaze.pow
+curl -o %HOMEDRIVE%\power_unlock.reg https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/power_unlock.reg
+curl -o %HOMEDRIVE%\NO_OC_WinOptiMaze.pow https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/NO_OC_WinOptiMaze.pow
+curl -o %HOMEDRIVE%\Balanced_WinOptiMaze.pow https://raw.githubusercontent.com/ny4rlk0/WinOptiMaze/refs/heads/main/Balanced_WinOptiMaze.pow
 powershell -command "([regex]::Matches((powercfg -list), 'GUID: ([\w-]+)') | ForEach-Object { $_.Groups[1].Value }) | ForEach-Object { powercfg -delete $_ }"
-regedit /s power_unlock.reg
-powershell -command "powercfg /import NO_OC_WinOptiMaze.pow"
+regedit /s %HOMEDRIVE%\power_unlock.reg
+powershell -command "powercfg /import %HOMEDRIVE%\NO_OC_WinOptiMaze.pow"
 timeout /t 1
-powershell -command "powercfg /import Balanced_WinOptiMaze.pow"
+powershell -command "powercfg /import %HOMEDRIVE%\Balanced_WinOptiMaze.pow"
 for /f "tokens=4" %%f in ('powercfg -list ^| findstr /C:"NO_OC_WinOptiMaze"') do set GUID=%%f
 powercfg /S %GUID%
 pause
-del power_unlock.reg
-del NO_OC_WinOptiMaze.pow
-del Balanced_WinOptiMaze.pow
+del %HOMEDRIVE%\power_unlock.reg
+del %HOMEDRIVE%\NO_OC_WinOptiMaze.pow
+del %HOMEDRIVE%\Balanced_WinOptiMaze.pow
 start /b "" cmd /c del "%~f0"&exit /b
 ::Kodu buraya yaz:: 
