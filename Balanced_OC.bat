fc2cefdab7b6ebbeddfbef00d5d603b7292f67de
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2025-05-31 15:01:15 +0300

    Update Balanced_OC.bat

--- Balanced_OC.bat
+++ Balanced_OC.bat
@@ -1,12 +1,27 @@
 @echo off
-TITLE WinOptiMaze v0.1 
+TITLE WinOptiMaze v1.0 
 cls
-echo github.com/ny4rlk0/WinOptiMaze
-echo _______________________________________________________________
+mode con: cols=70 lines=22
+color F0
+cls
+echo ----------------------------------------------------------------------
+echo "     __        ___        ___        _   _ __  __                  ";
+echo "     \ \      / (_)_ __  / _ \ _ __ | |_(_)  \/  | __ _ _______    ";
+echo "      \ \ /\ / /| | '_ \| | | | '_ \| __| | |\/| |/ _` |_  / _ \   ";
+echo "       \ V  V / | | | | | |_| | |_) | |_| | |  | | (_| |/ /  __/   ";
+echo "        \_/\_/  |_|_| |_|\___/| .__/ \__|_|_|  |_|\__,_/___\___|   ";
+echo "                              |_|                                  ";
+echo "                   github.com/ny4rlk0/WinOptiMaze                  ";
+echo ----------------------------------------------------------------------
+echo              Set power settings to balanced Over Clock!
+echo ----------------------------------------------------------------------
 if "%~1" neq "skip" goto getAdmin
 :getAdmin
 net session >nul 2>&1
 if %errorlevel% neq 0 (
+	mode con: cols=70 lines=22
+	color F0
+	cls
 	echo %~nx0: Requesting Administrative privileges.
 	echo %~nx0: Adjusting system for Balanced OVERCLOCK.
 	powershell -Command "Start-Process '%~f0' -ArgumentList 'skip' -Verb RunAs"
